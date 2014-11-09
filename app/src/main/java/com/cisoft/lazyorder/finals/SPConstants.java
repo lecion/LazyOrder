2de3da98ef61c2611ddb0274ca1261ca799b820a
comet <921538120@qq.com>
2014-11-09 17:21:45 +0800

    添加一个常量文件，后期会紧随新增功能

---
+++ app/src/main/java/com/cisoft/lazyorder/finals/SPConstants.java
@@ -0,0 +1,24 @@
+package com.cisoft.lazyorder.finals;
+
+/**
+ * Created by comet on 2014/11/6.
+ */
+public class SPConstants {
+
+    public static final String SP_FILE_NAME = "sp_lazyorder";
+
+    //是否是第一次安装
+    public static final String KEY_IS_FIRST_INSTALL = "isFirstInstall";
+    //最近的一次保存的姓名
+    public static final String KEY_RECENT_NAME = "recentName";
+    //最近的一次保存的手机号码
+    public static final String KEY_RECENT_PHONE_NUM = "recentPhoneNum";
+    //最近一次保存的地址
+    public static final String KEY_RECENT_BUILD_ID = "recentBuildId";
+    public static final String KEY_RECENT_BUILD_NAME = "recentBuildName";
+    public static final String KEY_RECENT_ROOM_NUM = "recentRoomNum";
+    //最近一次保存的付款方式
+    public static final String KEY_RECENT_PAY_WAY = "recentPayWay";
+    //最近一次保存的学校id
+    public static final String KEY_RECENT_SCHOOL_ID = "recentSchoolId";
+}
