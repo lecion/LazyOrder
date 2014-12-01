e5238c4927870ed7cf45021798829f0ae9fa941d
lecion <ylc931116@gmail.com>
2014-12-01 19:31:22 +0800

    添加商店Module

---
+++ shop/src/main/java/com/cisoft/shop/MainActivity.java
@@ -0,0 +1,49 @@
+package com.cisoft.shop;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.cisoft.myapplication.R;
+import com.igexin.sdk.PushManager;
+
+import org.kymjs.aframe.ui.activity.BaseActivity;
+
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public void setRootView() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        PushManager.getInstance().initialize(this.getApplicationContext());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
