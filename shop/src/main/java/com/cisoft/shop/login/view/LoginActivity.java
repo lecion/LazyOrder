abdaaf6e064c1180c30e6805dba01452d4937954
lecion <ylc931116@gmail.com>
2015-01-30 19:47:45 +0800

    添加登陆页面

---
+++ shop/src/main/java/com/cisoft/shop/login/view/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.cisoft.shop.login.view;
+
+import com.cisoft.myapplication.R;
+
+import org.kymjs.aframe.ui.activity.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    public void setRootView() {
+        setContentView(R.layout.activity_login);
+    }
+}
