d735a5be86e0e356de451467b41452df5dcc523a
lecion <ylc931116@gmail.com>
2015-01-30 21:25:06 +0800

    添加登陆动画效果

--- shop/src/main/java/com/cisoft/shop/login/view/LoginActivity.java
+++ shop/src/main/java/com/cisoft/shop/login/view/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.cisoft.shop.login.view;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.cisoft.myapplication.R;
@@ -14,6 +19,12 @@ public class LoginActivity extends BaseActivity {
     @BindView(id = R.id.iv_app_logo)
     private ImageView ivAppLogo;
 
+    @BindView(id = R.id.et_phone)
+    private EditText etPhone;
+
+    @BindView(id = R.id.et_pwd)
+    private EditText etPwd;
+
     @Override
     public void setRootView() {
         setContentView(R.layout.activity_login);
@@ -21,7 +32,26 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     protected void initWidget() {
-        ObjectAnimator animator = ObjectAnimator.ofFloat(ivAppLogo, "translationY", DeviceUtil.getScreenHeight(this) / 4 * 3, 0).setDuration(1000);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(ivAppLogo, "translationY", DeviceUtil.getScreenHeight(LoginActivity.this), 0, DeviceUtil.getScreenHeight(LoginActivity.this) / 10, 0);
+//        animator.setStartDelay(200);
+        animator.setDuration(2500);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                ValueAnimator anim = ValueAnimator.ofFloat(0, 1);
+                anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        float value = (float) animation.getAnimatedValue();
+                        etPhone.setAlpha(value);
+                        etPwd.setAlpha(value);
+                    }
+                });
+                anim.setDuration(700);
+                anim.start();
+            }
+        });
         animator.start();
     }
 }
